c0d472270e541b4e0cb9108dbdfc014ba9f32e0b
Jikky1618 <Jikky0110@gmail.com>
2022-04-15 00:43:03 +0900

    Fix: toggle method

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(function() {
     const hum = $('#hamburger, #close')
-    const nav = $('#sp-nav')
     hum.on('click', function(){
-        alert("クリックされました");
+        $('#sp-nav').toggleClass('toggle');
     });
 });
