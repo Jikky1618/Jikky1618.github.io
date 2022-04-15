bcb1459ccb6418d281306f115b264c223cff9181
Jikky1618 <Jikky0110@gmail.com>
2022-04-15 22:12:34 +0900

    Add click function

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-$(function() {
-    const hum = $('#hamburger, #close')
-    $('#hamburger').on('click', function(){
-        alert('hamburger clicked');
-        console.log('hamburger clicked');
-    });
+$(".hamburger-btn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".hamburger-btn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
