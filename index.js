101c125845c77cc7617aa7c95c4365fe16b4351b
Jikky1618 <Jikky0110@gmail.com>
2022-04-17 00:05:08 +0900

    Delete:comment

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-$(".hamburger-btn").click(function () {//ボタンがクリックされたら
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+$(".hamburger-btn").click(function () {
+	$(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+    $(".circle-bg").toggleClass('circleactive');
 });
 
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".hamburger-btn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
-    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+$("#g-nav a").click(function () {
+    $(".hamburger-btn").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
+    $(".circle-bg").removeClass('circleactive');
 });
