5f981d3dbe0283e2c29c2236a0d75767a726795e
Jikky1618 <Jikky0110@gmail.com>
2022-04-24 01:13:41 +0900

    Add: link scroll

--- index.js
+++ index.js
@@ -24,3 +24,10 @@ $(window).scroll(function() {
 });
 
 
+$('a[href*="#"]').click(function () {
+    var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+    var pos = $(elmHash).offset().top;  //idの上部の距離を取得
+    $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+    return false;
+  });
+
