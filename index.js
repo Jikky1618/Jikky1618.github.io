95b707057968c857d8d107005567dda4ba1cc8d5
Jikky1618 <Jikky0110@gmail.com>
2022-04-24 02:29:13 +0900

    Fix: scroll position

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ $(window).scroll(function() {
 
 
 $('a[href*="#"]').click(function () {
-    var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-    var pos = $(elmHash).offset().top;  //idの上部の距離を取得
-    $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+    var elmHash = $(this).attr('href');
+    var pos = $(elmHash).offset().top-100;
+    $('body,html').animate({scrollTop: pos}, 500);
     return false;
   });
 
