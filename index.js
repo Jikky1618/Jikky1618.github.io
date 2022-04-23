21e325337912e279c4e26b47b2a10f75eb739df1
Jikky1618 <Jikky0110@gmail.com>
2022-04-24 03:47:22 +0900

    Fix: scroll animation

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ $(window).scroll(function() {
 $('a[href*="#"]').click(function () {
     var elmHash = $(this).attr('href');
     var pos = $(elmHash).offset().top-100;
-    $('body,html').animate({scrollTop: pos}, 500);
+    $('body,html').animate({scrollTop: pos}, 250);
     return false;
   });
 
